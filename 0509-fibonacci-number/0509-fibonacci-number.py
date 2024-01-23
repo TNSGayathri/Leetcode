f0f2ac0ec4265eddb6197b314185fcf90a352a05
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2024-01-23 22:45:16 +0530

    Time: 397 ms (18.65%), Space: 16.5 MB (60.58%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if(n==0 or n==1):
+            return n
+        sn=self.fib(n-1)
+        sn1=self.fib(n-2)
+        return sn+sn1
+        
