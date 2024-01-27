3e2eabcd2b4b9c557f160cd0bb76d8cd36f140de
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2024-01-27 10:44:07 +0530

    Time: 900 ms (5.02%), Space: 362.3 MB (5.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    void comb(int i,int n,int k,vector<int>&cm,vector<vector<int>>&v,vector<int>&a)
+    vector<vector<int>>comb(int i,int n,int k,vector<int>&cm,vector<vector<int>>&v,vector<int>&a)
     {
         if(i==n){
             if(k==0)
             {
                 v.push_back(cm);
-                return ;
             }
-            return ;
+            return v ;
         }
         if(a[i]<=k){
             cm.push_back(a[i]);
@@ -18,13 +17,14 @@ public:
             cm.pop_back();
         }
         comb(i+1,n,k,cm,v,a);
+        return v;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>v;
         vector<int>cm;
         int n=candidates.size();
         comb(0,n,target,cm,v,candidates);
-        return v;
         
+        return v;
     }
 };
